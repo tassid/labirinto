c319720568934991e4678d3e5083a0e526226667
tassiane anzolin <tassianeanzolin@alunos.utfpr.edu.br>
2023-11-05 13:36:02 -0300

    ajustes de tile

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+const MAZE_WIDTH = 80;
+const MAZE_HEIGHT = 60;
+
+const VISIBLE_AREA = {
+  x: 0,
+  y: 0,
+  width: MAZE_WIDTH,
+  height: MAZE_HEIGHT,
+};
+
+const terrainToImage = {
+  grass: 'grass',
+  sand: 'sand',
+  rocky: 'rocky',
+  wall: 'wall',
+  swamp: 'swamp',
+  treasure: 'treasure',
+};
+
 const config = {
   type: Phaser.AUTO,
-  width: 1900,
-  height: 1000,
+  width: 800,
+  height: 600,
   scene: {
     preload: preload,
     create: create,
@@ -10,30 +29,18 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-// Defina a área visível (por exemplo, dentro do retângulo 400x400 no centro)
-const visibleArea = {
-  x: 200, // Largura do retângulo visível
-  y: 100, // Altura do retângulo visível
-  width: 400, // Largura do retângulo visível
-  height: 400, // Altura do retângulo visível
-};
-
 function preload() {
-  // Carregue as imagens do seu conjunto de azulejos (ex: parede, areia, rochosa, grama, pântano, tesouro)
-  this.load.image('wall', 'assets/wall.png');
-  this.load.image('sand', 'assets/sand.png');
-  this.load.image('rocky', 'assets/rocky.png');
-  this.load.image('grass', 'assets/grass.png');
-  this.load.image('swamp', 'assets/swamp.png');
-  this.load.image('treasure', 'assets/treasure.png');
+  for (const terrain in terrainToImage) {
+    this.load.image(terrain, `assets/${terrain}.png`);
+  }
 }
 
 function generateRandomMaze(width, height) {
   const maze = new Array(width);
 
   const percentages = {
-    grass: 0.70,  // Aumente a porcentagem de grama
-    sand: 0.15,   // Reduza a porcentagem de areia
+    grass: 0.70,
+    sand: 0.15,
     rocky: 0.05,
     swamp: 0.05,
     wall: 0.05,
@@ -63,59 +70,43 @@ function generateRandomMaze(width, height) {
     }
   }
 
-  // Encontre uma localização aleatória para o tesouro dentro da área visível
   let treasureX, treasureY;
   do {
-    treasureX = visibleArea.x + Math.floor(Math.random() * visibleArea.width);
-    treasureY = visibleArea.y + Math.floor(Math.random() * visibleArea.height);
+    treasureX = VISIBLE_AREA.x + Math.floor(Math.random() * VISIBLE_AREA.width);
+    treasureY = VISIBLE_AREA.y + Math.floor(Math.random() * VISIBLE_AREA.height);
   } while (maze[treasureX][treasureY] === 'wall');
 
-  // Marque a localização do tesouro
   maze[treasureX][treasureY] = 'treasure';
 
   return maze;
 }
 
 function create() {
-  // Gere um labirinto aleatório com diferentes terrenos e um tesouro
-  const maze = generateRandomMaze(80, 60);
+  const maze = generateRandomMaze(MAZE_WIDTH, MAZE_HEIGHT);
+  const tileSize = 40;
 
-  // Defina o tamanho de cada azulejo
-  const tileSize = 10;
-
-  // Renderize o labirinto com base nos dados de terreno
-  for (let x = visibleArea.x; x < visibleArea.x + visibleArea.width; x++) {
-    for (let y = visibleArea.y; y < visibleArea.y + visibleArea.height; y++) {
-      const terrain = maze[x][y];
-      let tile;
-
-      switch (terrain) {
-        case 'grass':
-          tile = this.add.image((x - visibleArea.x) * tileSize, (y - visibleArea.y) * tileSize, 'grass');
-          break;
-        case 'sand':
-          tile = this.add.image((x - visibleArea.x) * tileSize, (y - visibleArea.y) * tileSize, 'sand');
-          break;
-        case 'rocky':
-          tile = this.add.image((x - visibleArea.x) * tileSize, (y - visibleArea.y) * tileSize, 'rocky');
-          break;
-        case 'wall':
-          tile = this.add.image((x - visibleArea.x) * tileSize, (y - visibleArea.y) * tileSize, 'wall');
-          break;
-        case 'swamp':
-          tile = this.add.image((x - visibleArea.x) * tileSize, (y - visibleArea.y) * tileSize, 'swamp');
-          break;
-        case 'treasure':
-          tile = this.add.image((x - visibleArea.x) * tileSize, (y - visibleArea.y) * tileSize, 'treasure');
-          // Imprima as coordenadas do tesouro
-          const treasureCoords = this.add.text((x - visibleArea.x) * tileSize, (y - visibleArea.y) * tileSize, `X: ${x}, Y: ${y}`, { fontSize: '16px', fill: '#FF0000' });
-          break;
-        default:
-          // Lidar com qualquer outro tipo de terreno, conforme necessário
-          break;
+  for (let x = VISIBLE_AREA.x; x < VISIBLE_AREA.x + VISIBLE_AREA.width; x++) {
+    for (let y = VISIBLE_AREA.y; y < VISIBLE_AREA.y + VISIBLE_AREA.height; y++) {
+      if (isInsideVisibleArea(x, y)) {
+        const terrainName = maze[x][y];
+        const imageName = terrainToImage[terrainName];
+        
+        if (imageName) {
+          const tile = this.add.image((x - VISIBLE_AREA.x) * tileSize, (y - VISIBLE_AREA.y) * tileSize, imageName);
+          
+          if (terrainName === 'treasure') {
+            const treasure = this.add.image((x - VISIBLE_AREA.x) * tileSize, (y - VISIBLE_AREA.y) * tileSize, 'treasure');
+            treasure.setScale(tileSize / treasure.width); // Ajuste a escala para corresponder ao tamanho do tile
+            console.log(`Tesouro em X: ${x}, Y: ${y}`);
+          }
+        }
       }
     }
   }
+}
+
+function isInsideVisibleArea(x, y) {
+  return x >= VISIBLE_AREA.x && x < VISIBLE_AREA.x + VISIBLE_AREA.width && y >= VISIBLE_AREA.y && y < VISIBLE_AREA.y + VISIBLE_AREA.height;
 }
 
 function getRandomTerrain(percentages) {
@@ -129,6 +120,5 @@ function getRandomTerrain(percentages) {
     }
   }
 
-  // Recorra a 'sand' se houver problemas com as porcentagens
   return 'sand';
 }
